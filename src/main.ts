09b09564e14bb5744dbe0cbb42d3b6bab0a95aa7
Frikk Balder <33499052+MindChirp@users.noreply.github.com>
2024-04-03 13:22:05 +0200

    formkit conifg

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,16 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-
+// \/ FIXME - Cannot pass a custom config to formkit for some reason.
+// import config from 'formkit.config.ts'
 import App from './App.vue'
 import router from './router'
+import { defaultConfig, plugin } from '@formkit/vue'
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(plugin, defaultConfig /*(config)*/)
 
 app.mount('#app')
