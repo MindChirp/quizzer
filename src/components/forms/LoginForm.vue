4f4e0fc3e82d3afffc7f3f8f50643eb7e1d4f8ad
Honrik <henrikgnilsen02@gmail.com>
2024-04-04 13:23:59 +0200

    Created log in component

---
+++ src/components/forms/LoginForm.vue
@@ -0,0 +1,49 @@
+<script setup lang="ts">
+import axios from 'axios'
+
+interface User {
+  username: string;
+  password: string;
+}
+
+const loginUser = (data:User) => {
+  const newUser: User = {
+    username: data.username,
+    password: data.password,
+  }
+
+  axios.post('http://localhost:8080/api/users/login', newUser)
+      .then(response => {
+        console.log("Login status: ", response.data);
+      })
+      .catch(error => {
+        console.log("There was an error:", error);
+      })
+}
+</script>
+<template>
+
+  <h1>Log in</h1>
+  <FormKit type="form" @submit="loginUser">
+    <FormKit
+        type="text"
+        name="username"
+        id="username"
+        validation="required"
+        label="Username"
+        placeholder="SteveCraft123"
+    />
+
+    <FormKit
+        type="password"
+        name="password"
+        id="password"
+        validation="required"
+        label="Password"
+    />
+  </FormKit>
+
+</template>
+<style scoped>
+
+</style>
