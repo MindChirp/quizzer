f8f3ec2a521833cc6783b19579fb48d517c89eba
Honrik <henrikgnilsen02@gmail.com>
2024-04-04 21:49:04 +0200

    Made httputils a typescript file for better import to token.ts

--- src/utils/httputils.ts
+++ src/utils/httputils.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const baseurl = "http://localhost:8080";
 
-export const getJwtToken = (username, password) => {
+export const getJwtToken = (username:string, password:string) => {
   const config = {
     headers: {
       "Content-Type": "application/json",
@@ -16,7 +16,7 @@ export const getJwtToken = (username, password) => {
   return axios.post(baseurl + "/token", JSON.stringify(body), config);
 }
 
-export const getUserInfo = (username, token) => {
+export const getUserInfo = (username:string, token:string) => {
   const config = {
     headers: {
       "Authorization" : "Bearer " + token
