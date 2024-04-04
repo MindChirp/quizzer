dab72712f5d2dcdf6e6e3c211cc7019f8c10500d
Honrik <henrikgnilsen02@gmail.com>
2024-04-04 13:42:16 +0200

    Created login view

---
+++ src/views/LoginView.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="login-view">
+    <LoginComponent />
+  </div>
+</template>
+
+<script setup lang="ts">
+import LoginComponent from '../components/forms/LoginForm.vue';
+</script>
+
+<style scoped>
+.login-view {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+}
+</style>
