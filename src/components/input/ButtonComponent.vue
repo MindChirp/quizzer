3ba5607278df2c9735d1dec36ba89e9f6c50bdc7
Frikk Balder <33499052+MindChirp@users.noreply.github.com>
2024-04-04 14:45:54 +0200

    removed unused import

--- src/components/input/ButtonComponent.vue
+++ src/components/input/ButtonComponent.vue
@@ -1,6 +1,5 @@
 <script setup lang="ts">
 import type { ComponentSizes, ComponentVariants } from '@/types/component.ts'
-import ToasterComponent from '@/components/data/ToasterComponent.vue'
 
 interface ButtonProps {
   variant?: ComponentVariants
