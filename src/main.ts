c4b2014eeb52338567dfa6065a927a5eaf8b872b
Frikk Balder <33499052+MindChirp@users.noreply.github.com>
2024-04-06 13:21:25 +0200

    Fixed build error tihi

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,13 @@
 import './assets/main.css'
 
-import 'primevue/resources/themes/aura-light-green/theme.css'
-
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 import { defaultConfig, plugin } from '@formkit/vue'
 import config from './formkit.config.ts'
-import PrimeVue from 'primevue/config'
 const app = createApp(App)
 app.use(createPinia())
 app.use(router)
-app.use(PrimeVue)
 app.use(plugin, defaultConfig(config))
 app.mount('#app')
