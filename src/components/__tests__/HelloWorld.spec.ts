287fe9ed71d1c876ad7591702ecb8a30c658ea76
Frikk Balder <33499052+MindChirp@users.noreply.github.com>
2024-04-08 07:39:07 +0200

    tests

--- src/components/__tests__/HelloWorld.spec.ts
+++ src/components/__tests__/HelloWorld.spec.ts
@@ -2,10 +2,11 @@ import { describe, it, expect } from 'vitest'
 
 import { mount } from '@vue/test-utils'
 import HelloWorld from '../HelloWorld.vue'
+import EditImageModal from '../data/EditImageModal.vue'
 
 describe('HelloWorld', () => {
   it('renders properly', () => {
-    const wrapper = mount(HelloWorld, { props: { msg: 'Hello Vitest' } })
+    const wrapper = mount(EditImageModal, { props: { msg: 'Hello Vitest' } })
     expect(wrapper.text()).toContain('Hello Vitest')
   })
 })
