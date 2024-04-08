063ab9645e2d64bf5295a39228d8cd1c3d8cfecb
Honrik <henrikgnilsen02@gmail.com>
2024-04-08 07:50:31 +0200

    Made modal -> renders when open test work

--- src/components/__tests__/HelloWorld.spec.ts
+++ src/components/__tests__/HelloWorld.spec.ts
@@ -1,12 +1,13 @@
 import { describe, it, expect } from 'vitest'
-
 import { mount } from '@vue/test-utils'
-import HelloWorld from '../HelloWorld.vue'
-import EditImageModal from '../data/EditImageModal.vue'
+import ModalComponent from '@/components/data/ModalComponent.vue'
 
-describe('HelloWorld', () => {
-  it('renders properly', () => {
-    const wrapper = mount(EditImageModal, { props: { msg: 'Hello Vitest' } })
-    expect(wrapper.text()).toContain('Hello Vitest')
+describe('ModalComponent', () => {
+  // Test if the component renders when the `open` prop is true
+  it('renders when open', () => {
+    const wrapper = mount(ModalComponent, {
+      props: { open: true, title: 'Set image source' }
+    })
+    expect(wrapper.findComponent(ModalComponent).exists()).toBe(true)
   })
 })
